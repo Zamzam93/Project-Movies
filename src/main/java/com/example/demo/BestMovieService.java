773f2c14fe8920b0d64b47be225d17828bc5c867
Zamzam93 <samira0101@live.com>
2022-10-06 10:01:33 -0500

    part1 of Movie completed

--- src/main/java/com/example/demo/BestMovieService.java
+++ src/main/java/com/example/demo/BestMovieService.java
@@ -1,16 +1,33 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BestMovieService {
-    @Autowired
+
     private Movie movie;
 
-    public Movie getBestMovie(){
+    @Autowired
+    public BestMovieService(@Qualifier("titanicMovie") Movie movie)
+    {
+        this.movie = movie;
+    }
+
+    public Movie getBestMovie() {
         return movie;
     }
+
+
+
+
+
+
+
+
+
+
 
 
 }
